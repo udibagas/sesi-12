91b89d394ad6fe4dbd6122108c518a3d4d26d092
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-08-22 21:38:50 +0700

    update

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 app.use("/api/v1", require("./routes"));
 
 // error handling
